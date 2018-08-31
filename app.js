99937df0c7fbb233e9e2cccfa1628245697862c5
Sean <Spglancy@gmail.com>
2018-08-31 09:50:48 -0700

    users can see reviews

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
-
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('reviews-index', { reviews: reviews });
 })
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
