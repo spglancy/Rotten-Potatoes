c24a73031e4da2ba89bb00bbf63e7b055af4415d
Sean <Spglancy@gmail.com>
2018-09-04 14:48:35 -0700

    added basic styling

--- app.js
+++ app.js
@@ -3,17 +3,19 @@ const app = express();
 var exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 mongoose.connect('mongodb://localhost/rotten-potatoes');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 const Review = mongoose.model('Review', {
   title: String,
   description: String,
   movieTitle: String,
-  number: String
+  rating: String
 });
 
 app.get('/', (req, res) => {
@@ -34,8 +36,33 @@ app.post('/reviews', (req, res) => {
   })
 })
 
+app.put('/reviews/:id', (req, res) => {
+  Review.findByIdAndUpdate(req.params.id, req.body)
+    .then(review => {
+      res.redirect(`/reviews/${review._id}`)
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+app.delete('/reviews/:id', function (req, res) {
+  console.log("DELETE review")
+  Review.findByIdAndRemove(req.params.id).then((review) => {
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
 app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
+})
+
+app.get('/reviews/:id/edit', function (req, res) {
+  Review.findById(req.params.id, function(err, review) {
+    res.render('reviews-edit', {review: review});
+  })
 })
 
 app.get('/reviews/:id', (req, res) => {
