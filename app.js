eb47ea144f821536e8f01618a7974e30b5adb779
Sean <Spglancy@gmail.com>
2018-09-04 10:22:42 -0700

    added mongodb

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 var exphbs = require('express-handlebars');
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+mongoose.connect('mongodb://localhost/rotten-potatoes');
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const Review = mongoose.model('Review', {
+  title: String
+});
+
+app.post('/reviews', (req, res) => {
+  console.log(req.body);
+  // res.render('reviews-new', {});
+})
+
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find().then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch((err) => {
+      console.log(err);
+    })
 })
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
